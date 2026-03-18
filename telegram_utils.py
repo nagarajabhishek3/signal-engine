b8fdd7dd01fea51cd199852a8445c770d258d1e2
nagarajabhishek3 <nagarajabhishek3@gmail.com>
2026-03-18 11:25:43 +0530

    Create telegram_utils.py

---
+++ telegram_utils.py
@@ -0,0 +1,19 @@
+import requests
+
+BOT_TOKEN = "8611656592:AAH1i1uQYCsYuioAeABBfAjN_3qmm7gIQII"
+CHAT_ID = "974300325"
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+
+    try:
+        response = requests.post(url, data=payload)
+        return response.json()
+    except Exception as e:
+        print("Telegram Error:", e)
